cfcbd09c3d38f1b753c1e2ce19278efa08335f0a
RobertoVillani <robertovillani58@gmail.com>
2021-11-19 15:01:06 +0100

    add name, role e img

---
+++ js/script.js
@@ -0,0 +1,65 @@
+const teamContainer = document.querySelector('.team-container');
+
+const team = [
+    {
+        'name': 'Wayne Barnett',
+        'role': 'Founder & CEO',
+        'img': 'wayne-barnett-founder-ceo.jpg',
+    },
+
+    {
+        'name': 'Angela Caroll',
+        'role': 'Chief Editor',
+        'img': 'angela-caroll-chief-editor.jpg',
+    },
+
+    {
+        'name': 'Walter Gordon',
+        'role': 'Office Manager',
+        'img': 'walter-gordon-office-manager.jpg',
+    },
+
+    {
+        'name': 'Angela Lopez',
+        'role': 'Social Media Manager',
+        'img': 'angela-lopez-social-media-manager.jpg',
+    },
+
+    {
+        'name': 'Scott Estrada',
+        'role': 'Developer',
+        'img': 'scott-estrada-developer.jpg',
+    },
+
+    {
+        'name': 'Barbara Ramos',
+        'role': 'Graphic Designer',
+        'img': 'barbara-ramos-graphic-designer.jpg',
+    }
+
+];
+
+const btnAdd = document.getElementById("addMemberButton");
+
+console.log("Membri del team:");
+stampaCard(team, teamContainer);
+
+function stampaCard(array, container) {
+    container.innerHTML = "";
+    for (i = 0; i < array.length; i++) {
+        container.innerHTML += `<div class="team-card">
+        <div class="card-image">
+          <img
+            src="img/${array[i].img}"
+          />
+        </div>
+        <div class="card-text">
+          <h3>${array[i].name}</h3>
+          <p>${array[i].role}</p>
+        </div>
+      </div>`;
+
+        console.log(`${array[i].name} - ${array[i].role}`);
+    }
+}
+
